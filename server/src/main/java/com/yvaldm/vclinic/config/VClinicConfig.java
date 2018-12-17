fd8907e24e49ba111ebcfd828217b1f7b08cf1ce
Valery Yakovlev <vd.yakovlev@mpoffice.ru>
2018-12-17 20:33:13 +0300

    web security added

---
+++ server/src/main/java/com/yvaldm/vclinic/config/VClinicConfig.java
@@ -0,0 +1,14 @@
+package com.yvaldm.vclinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * VClinic Application Configuration
+ *
+ * @author valeryyakovlev
+ */
+@Configuration
+@EnableWebSecurity
+public class VClinicConfig {
+}
--- server/src/test/java/com/yvaldm/vclinic/controller/TestControllerIT.java
+++ server/src/test/java/com/yvaldm/vclinic/controller/TestControllerIT.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
+@WithMockUser
 public class TestControllerIT {
 
     @Autowired
