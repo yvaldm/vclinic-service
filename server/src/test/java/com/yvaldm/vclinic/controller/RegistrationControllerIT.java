06c4908ecc72d626cf9d4507831322331b7d31d1
Valery Yakovlev <vd.yakovlev@mpoffice.ru>
2018-12-20 00:10:27 +0300

    csrf disabled

--- server/src/test/java/com/yvaldm/vclinic/controller/RegistrationControllerIT.java
+++ server/src/test/java/com/yvaldm/vclinic/controller/RegistrationControllerIT.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -21,18 +21,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-@WithMockUser
-public class TestControllerIT {
+@WithMockUser(username = "some", roles = {"ADMIN"}, password = "pwd")
+public class RegistrationControllerIT {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void shouldCreateCar() throws Exception {
+    public void shouldCreateRegistrationApplication() throws Exception {
 
         // act
-        mvc.perform(get("/test"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.result").value("some"));
+        mvc.perform(post("/registration/apply")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
     }
 }
